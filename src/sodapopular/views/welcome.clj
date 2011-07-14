4eaaeea45e6c43b14a3cc4a011b7f6a5b1eb8d9a
Jaecen <jason@addington.com>
2011-07-14 15:44:06 -0700

    Fixed welcome.cj

--- src/sodapopular/views/welcome.clj
+++ src/sodapopular/views/welcome.clj
@@ -1,10 +1,11 @@
 (ns sodapopular.views.welcome
-    (:require [sodapopular.views.common :as common])
-    (use noir.core
-        hiccup.core
-        hiccup.page-helpers))
+  (:require [sodapopular.views.common :as common]
+            [noir.content.pages :as pages])
+  (use noir.core
+       hiccup.core
+       hiccup.page-helpers))
 
 (defpage "/welcome" []
     (common/layout
-        [:h1 "Welcome to SodaPopular"]
-        [:p "Prepare for the democratization of pop distribution"))
+        [:h1 "Welcome to sodapopular"]
+        [:p "Prepare for the democratization of carbonated libation"]))
